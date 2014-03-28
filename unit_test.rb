3b59c9538bcd295e39e216935382863b9c15f862
Olle K <olle.kvarnstroem@gmail.com>
2014-03-28 13:23:37 +0100

    added shebang, switched stuff

--- unit_test.rb
+++ unit_test.rb
@@ -1,3 +1,5 @@
+#! /usr/bin/env ruby
+
 require 'test/unit'
 require './pseudocode.rb'
 
@@ -5,15 +7,10 @@ class TestPseudoCode < Test::Unit::TestCase
   def test_tokens
     pc = PseudoCode.new
     assert_equal(nil, pc.parse(""))
-    puts "ASSERTING write(1.2)"
     assert_equal(1.2, pc.parse("write 1.2"))
-    puts "ASSERTING write(1)"
     assert_equal(1, pc.parse("write 1"))
-    puts "ASSERTING write(true)"
-    assert_equal(true, pc.parse("write true or false"))
-    puts "ASSERTING write(true)"
     assert_equal(true, pc.parse("write true"))
-    puts "ASSERTING write(\"hej\")"
     assert_equal("\"hej\"", pc.parse("write \"hej\""))
+    assert_equal(true, pc.parse("write true or false"))
   end
 end
